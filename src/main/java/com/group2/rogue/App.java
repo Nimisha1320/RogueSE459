29f0c98c6782e5fb68a7b4c43f9383e4cc6692b2
bramesh1 <bramesh1@depaul.edu>
2025-02-09 16:35:30 -0600

    updated player movement

--- src/main/java/com/group2/rogue/App.java
+++ src/main/java/com/group2/rogue/App.java
@@ -1,22 +1,29 @@
 package com.group2.rogue;
 
-import com.group2.rogue.worldgeneration.RogueLevel;
 import com.group2.rogue.worldgeneration.World;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        // System.out.println( "Hello World!" );
-        // RogueLevel.main(null);
-
+public class App {
+    public static void main(String[] args) {
         World world = new World();
         world.generateWorld();
-        
+        world.placePlayer();
 
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            world.displayWorld();
+            System.out.print("Move (WASD): ");
+            String input = scanner.nextLine().toUpperCase();
+
+            if (input.equals("Q")) { // Quit game
+                System.out.println("Exiting...");
+                break;
+            }
+
+            if (input.length() == 1) {
+                world.movePlayer(input.charAt(0));
+            }
+        }
+        scanner.close();
     }
 }
